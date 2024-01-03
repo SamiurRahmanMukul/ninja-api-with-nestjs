57ef94ce7db2b0e638a6580b8575f93811141857
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2024-01-03 15:39:58 +0600

    user module removew in ninnjs api's

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
-import { User } from './user/entities/user.entity';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'ninja-api-with-nestjs',
-      entities: [User],
-      synchronize: true,
-    }),
-    NinjasModule,
-    UserModule,
-  ],
+  imports: [NinjasModule],
   controllers: [AppController],
   providers: [AppService],
 })
